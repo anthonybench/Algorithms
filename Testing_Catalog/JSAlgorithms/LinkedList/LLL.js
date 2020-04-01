2d9f20db65afd989be523f68f08848ccc5cf1a92
SleepyBoy <anthonybenchyep@gmail.com>
2020-03-31 21:51:16 -0700

    automated push: Tue Mar 31 21:51:15 PDT 2020

---
+++ Testing_Catalog/JSAlgorithms/LinkedList/LLL.js
@@ -0,0 +1,76 @@
+//=============================================
+//=============================================
+// Linear Linked List
+// LLL.js
+//=============================================
+//=============================================
+// LLL node
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// Functionality
+class LLL {
+  constructor() {
+    this.head = null;
+  }
+
+  //add new node
+  add(data) {
+    const newNode = new Node(data);
+    
+    if (!this.head)
+      this.head = newNode;
+    else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    return;
+  }
+
+  //remove last node
+  clip() {
+    let current = this.head;
+
+    if (!this.head)
+      console.log("Empty list.");
+    else if (!this.head.next)
+      this.head = null;
+    else {
+      while(current.next.next)
+        current = current.next;
+      current.next = null;
+    }
+    return;
+  }
+
+  //display entire list
+  display() {
+    let current = this.head;
+    let res = "";
+
+    if (!this.head)
+      console.log("Emptly List");
+    else {
+      while (current) {
+        let buff = (current.data).toString();
+        res = res.concat(buff);
+        res = res.concat(" --> ");
+        current = current.next;
+      }
+      res = res.concat("NULL");
+      console.log(res);
+    }
+    return;
+  }
+
+}
+
+//exports enabled
+module.exports = {
+  Node,
+  LLL
+}
---
+++ Testing_Catalog/JSAlgorithms/LinkedList/tools.js
@@ -0,0 +1,38 @@
+//=============================================
+//=============================================
+// General Utility
+// tools.js
+//=============================================
+//=============================================
+
+// General Printing
+let border = function (c) {
+  if (typeof c != "string")
+    console.log("Invalid input. Use: 's', 'm', 'l' or 'd'");
+  else if (c.toLowerCase() == "s")
+    console.log("==============");
+  else if (c.toLowerCase() == "m")
+    console.log("==========================");
+  else if (c.toLowerCase() == "l")
+    console.log("===============================================");
+  else if (c.toLowerCase() == "d") {
+    console.log("===============================================");
+    console.log("===============================================");
+  }
+  return;
+};
+
+let space = function (n) {
+  if (n < 1)
+    console.log("Invalid input. Use int >= 0");
+  else
+    for (var i = 0; i < n; i++)
+      console.log("");
+  return;
+}
+
+//exports enabled
+module.exports = {
+  border,
+  space
+}
